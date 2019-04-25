2e9bfa262c2570ccbeaefa4859684d5178a569d9
s-oronae <s-oronae@52S500041226.it.bsd405.org>
2019-04-25 12:09:57 -0700

    upload blog test and other fixes and css

--- js.js
+++ js.js
@@ -1,14 +1,18 @@
-//let dropdownUp = false;
-//
-//$(document).ready(function() {
-//
-//	$('#menuIcon').click(function() {
-//		if (!dropdownUp) {
-//			$("#dropdownMenu").css("height","0px");
-//		} else {
-//			$("#dropdownMenu").css("height","52px");
-//		}
-//		dropdownUp = !dropdownUp;
-//	});
-//
-//});
+$(document).ready(function() {
+	$('.dropdownItem').click(function() {
+		location.replace($(this).text().toLowerCase() + ".html");
+	});
+	
+});
+
+fetch("./test.txt").then(function(a) {
+    if (200 !== a.status) {
+        alert("Looks like there was a problem. Status Code: " + a.status);
+        return;
+    }
+    a.text().then(function(a) {
+        alert(a);
+    });
+}).catch(function(a) {
+    alert("Fetch Error :-S", a);
+});
