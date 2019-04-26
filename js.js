5412d8aa358f50ca766cf22e221ce73e89e18394
s-oronae <s-oronae@52S500041226.it.bsd405.org>
2019-04-26 13:56:08 -0700

    Adding in good nav menu

--- js.js
+++ js.js
@@ -1,24 +1,46 @@
+let hidden = false;
+
 $(document).ready(function() {
-	$('.dropdownItem').click(function() {
-		location.replace($(this).text().toLowerCase() + ".html");
+	$(".dropdownItem:not(#hideDrop)").click(function() {
+		let href = $(this).children("a").attr('href');
+
+		location.replace($(this).children("a").attr('href'));
 	});
 	
+	$('#hideDrop').click(function(){
+		$('.dropdownItem:not(#hideDrop)').css("display",(hidden)?"initial":"none");
+		$("#downImg").attr("src","arrow"+((hidden)?"":"Down") + ".png");
+		$("#dropdown").toggleClass("dropdownUp");
+		hidden = !hidden;
+	});
+	
+	$(window).resize(function(){
+		if($(window).width() > 720){
+			$('.dropdownItem:not(#hideDrop)').css("display","inline");
+		}
+	  });
 });
 
 let blogInfo = "null";
 
-fetch("./blog/blogIndex.json").then(function(a) {
+let alert = false;
+
+fetch("https://oroarmor.github.io/blog/blogIndex.json")
+.then(function(a) {
     if (200 !== a.status) {
         alert("Looks like there was a problem. Status Code: " + a.status);
         return;
     }
     a.text().then(function(a) {
-    	alert("json time");
     	blogInfo = a;
+    	if(alert){
+    	alert("json time");
+    	
         alert(blogInfo);
+        alert(blogInfo.json());
+    	}
     });
-}).catch(function(a) {
-    //alert("Fetch Error :-S", a);
+})
+.catch(function(a) {
+    // alert("Fetch Error :-S", a);
 });
-
-
